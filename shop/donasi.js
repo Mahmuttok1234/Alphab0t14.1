d28765efa8b173636afce9b81a17da0544a89907
Mahmuttok1234 <89297825+Mahmuttok1234@users.noreply.github.com>
2022-01-25 11:58:47 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -15,13 +15,13 @@ return `DONASI BISA VIA BANK / E-MONEY
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 081217445262
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 081217445262
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 081217445262
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama Mahmut
 `
 	}
 
