c6a2a0c1f585d666a7a9cdecd054450c411cd9eb
Mahmuttok1234 <89297825+Mahmuttok1234@users.noreply.github.com>
2022-03-10 14:53:15 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -17,11 +17,11 @@ return `「 *PRICE LIST* 」
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6281217445262
 
 
 NOTE : 
-Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Bot On 24 Jam Karna Di Run Menggunakan RDP
 Reset Data Setiap Jam 02:00 `
 	}
 
